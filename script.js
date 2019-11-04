581b1280ad0b6620011254049ed76dcfec6abc19
Niraj Thing <linkinson7@gmail.com>
2019-11-04 18:31:02 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function showtime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    if(h < 10){
+        h = "0" + h;
+    }
+    else{
+        h = h;
+    }
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showtime, 1000);
+
+
+}
+showtime();
